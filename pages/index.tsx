4aaf2850ed05dfa65ccf13a8913f24d348174efa
Gustavo Santos <gefgu@hotmail.com>
2022-09-19 15:55:36 -0300

    Add logo in header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
@@ -10,7 +11,9 @@ const Home: NextPage = () => {
         <meta name="description" content="Rock Paper Scissors Game" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-asdfa
+      <header className={styles.header}>
+        <Image src="/logo.svg" width={162} height={99} />
+      </header>
     </div>
   );
 };
