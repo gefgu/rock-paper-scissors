8179f4e821543d8803d07c9d2bd362911dc6b663
Gustavo Santos <gefgu@hotmail.com>
2022-09-19 15:49:19 -0300

    Add background radial gradient

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,13 @@ import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <Head>
         <title>Rock Paper Scissors</title>
         <meta name="description" content="Rock Paper Scissors Game" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      Hello World
+asdfa
     </div>
   );
 };
